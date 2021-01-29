7ac52681e5f204ea923c3ae4bc62c32b3a1f8aa4
wimuka <avii08el1s@gmail.com>
2021-01-29 18:42:06 -0500

    Optimized Tailwind for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  purge: [
+    "./src/components/body.js",
+    "./src/components/contract.js",
+    "./src/components/footer.js",
+    "./src/components/gallery.js",
+    "./src/components/header.js",
+  ],
   theme: {
     extend: {},
   },
